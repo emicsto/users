ccde38cc48e35ce891e41ea35612b0a66b6afa07
emicsto <emicsto@gmail.com>
2021-03-19 21:55:20 +0100

    Remove id from User entity

--- src/main/java/com/emicsto/users/user/User.java
+++ src/main/java/com/emicsto/users/user/User.java
@@ -12,9 +12,6 @@ import javax.persistence.Id;
 @Entity
 class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String login;
 
     private int requestCount;
